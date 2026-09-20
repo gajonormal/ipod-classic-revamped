a40986ca0e1a21568cf949b4e4d718c435f56f4b
gajonormal <bernamaia12@gmail.com>
2026-09-20 15:43:03 +0100

    style: Hide browser scrollbars globally for a cleaner UI

--- app/components/Ipod/GlobalStyles.ts
+++ app/components/Ipod/GlobalStyles.ts
@@ -28,6 +28,12 @@ export const GlobalStyles = createGlobalStyle`
 
   * {
     box-sizing: border-box;
+    scrollbar-width: none; /* Firefox */
+    -ms-overflow-style: none; /* IE and Edge */
+  }
+
+  *::-webkit-scrollbar {
+    display: none; /* Chrome, Safari and Opera */
   }
 
   @media (display-mode: standalone) {
